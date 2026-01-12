6c74e9631ab65913fcb0e066a4a8185a0de8408b
Nikita <kuzliaev.nikita@yandex.ru>
2026-01-12 18:45:04 +0700

    add another one class for *Kafka Topic* & add simple controller with stub endpoint and autowire two `KafkaTopicInitializer`

---
+++ src/main/java/com/topic/controller/AsyncTest.java
@@ -0,0 +1,37 @@
+package com.topic.controller;
+
+import com.topic.dto.api.response.MessageResponse;
+import com.topic.kafka.KafkaTopicInitializer;
+import com.topic.util.annotations.Logging;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/async")
+public class AsyncTest {
+
+    private final KafkaTopicInitializer debugKafkaTopic;
+
+    private final KafkaTopicInitializer logKafkaTopic;
+
+    public AsyncTest(
+            @Qualifier("Kafka.Topics.DebugKafkaTopic")
+            KafkaTopicInitializer debugKafkaTopic,
+
+            @Qualifier("Kafka.Topics.LogKafkaTopic")
+            KafkaTopicInitializer logKafkaTopic
+    ) {
+        this.debugKafkaTopic = debugKafkaTopic;
+        this.logKafkaTopic = logKafkaTopic;
+    }
+
+
+    @GetMapping("/test")
+    @Logging
+    public MessageResponse processAsyncTask(){
+        return new MessageResponse("Fail"); // заглушка
+    }
+
+}
--- src/main/java/com/topic/kafka/topics/LogKafkaTopic.java
+++ src/main/java/com/topic/kafka/topics/LogKafkaTopic.java
@@ -8,24 +8,31 @@ import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.stereotype.Component;
 
 @KafkaTopic(
-        name = "debugTopic",
+        name = "logTopic",
         partitions = 1,
         replicationFactor = 1,
         retentionMs = "3600000" // 3600000 ms = 1 hour
 )
-@Component("Kafka.Topics.DebugKafkaTopic")
-public class DebugKafkaTopic implements KafkaTopicInitializer {
+@Component("Kafka.Topics.LogKafkaTopic")
+public class LogKafkaTopic implements KafkaTopicInitializer {
+
+
+    // todo: это еще один топик для теста - что спринг при запуске создает их два, и что они автовайрятся по именам
+    //
+
+    /*
+
+    COPIED FROM DebugKafkaTopic.java
+
+     */
 
     @Override
     public String getName() {
-        return "debugTopic";
+        return "logTopic";
     }
 
     @Override
     public NewTopic build() {
-
-        // todo: нехорошо, что одно и то же в аннтотации указано и здесь
-
         return TopicBuilder
                 .name(getName())
                 .partitions(1)
@@ -33,5 +40,4 @@ public class DebugKafkaTopic implements KafkaTopicInitializer {
                 .config(TopicConfig.RETENTION_MS_CONFIG, "3600000")
                 .build();
     }
-
 }
